1655b69c450a921f39dee2e55db3218523c75bd3
Mikus <mikusmeikulis@gmail.com>
2020-12-12 20:15:19 +0200

    changes

--- src/components/oneCell/oneCell.tsx
+++ src/components/oneCell/oneCell.tsx
@@ -18,11 +18,35 @@ const OneCell: FC<typeOneCell> = ({
     paddingTop: `${100 / gridSize}%`,
   };
 
+  const cellColor = {
+    color: '',
+  };
+
+  switch (cell) {
+    case 1:
+      break;
+    case 2:
+      cellColor.color = 'blue';
+      break;
+    case 3:
+      cellColor.color = 'green';
+      break;
+    case 4:
+      cellColor.color = 'purple';
+      break;
+    case 5:
+      cellColor.color = 'red';
+      break;
+    case 6:
+      cellColor.color = 'yellow';
+      break;
+  }
+
   return (
     <>
       {isOpen ? (
         <div className={style.cellOpen} onContextMenu={(e) => e.preventDefault()} style={cellStyle}>
-          <span className={style.cellOpenText}>
+          <span className={`${style.cellOpenText} `} style={cellColor}>
             {cell > 0 && cell}
             {cell === 'bomb' && <img className={style.image} src={bomb} alt="bomb" />}
           </span>
